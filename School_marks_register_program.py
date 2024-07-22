059f373a532e78a74280603f6e89e24c60297b37
kathirvel13 <kathir13@proton.me>
2024-07-22 21:04:35 +0530

    Bug fixes and new features

--- School_marks_register_program.py
+++ School_marks_register_program.py
@@ -1,33 +1,55 @@
+import csv
+
 classno = input("Enter class and section- ")
 
 f = open(f"{classno}_Student_List.csv", "a+", newline='')
-import csv
 wr = csv.writer(f)
 
 f.seek(0)
 if list(csv.reader(f)) == []:
-    wr.writerow(['name', 'roll_no', "English", "Maths", "Physics", "Chemistry", "Computer"])
+    wr.writerow(['Roll_no', 'Name', "English", "Maths", "Physics", "Chemistry", "Computer"])
+
+def get_rowelements(rownum):
+    # This function returns a list having all elements in a row when we mention the row's index number
+    data = list(csv.reader(f))[1:]
+    rowlst = []
+    for row in data:
+        rowlst.append(row[rownum])
+    return rowlst
+
+def unique_rollno(prompt):
+    rno = 0
+    rnolst = get_rowelements(0)
+    while rno in rnolst or rno <=0:
+        rno = int(input(prompt))
+    return rno
+
+def get_mark(prompt):
+    mark = -1
+    while mark > 100 or mark < 0:
+        mark = int(input(prompt))
+    return mark
 
 def add_student():
     no = int(input("Number of students to be added- "))
 
     for i in range(no):
-        name = input("Enter name of Student- ")
-        roll = int(input('Enter Roll number of Student- '))
-        eng = int(input('Enter English Mark of Student- '))
-        math = int(input('Enter Maths Mark of Student- '))
-        phy = int(input('Enter Physics Mark of Student- '))
-        chem = int(input('Enter Chemistry Mark of Student- '))
-        cs = int(input('Enter CS Mark of Student- '))
+        name = input(f"Enter name of Student {i+1}- ")
+        roll = unique_rollno(f'Enter Roll number of Student {i+1}- ')
+        eng = get_mark(f'Enter English Mark of Student {i+1}- ')
+        math = get_mark(f'Enter Maths Mark of Student {i+1}- ')
+        phy = get_mark(f'Enter Physics Mark of Student {i+1}- ')
+        chem = get_mark(f'Enter Chemistry Mark of Student {i+1}- ')
+        cs = get_mark(f'Enter CS Mark of Student {i+1}- ')
         print()
-        wr.writerow([name, roll, eng, math, phy, chem, cs])
+        wr.writerow([roll, name, eng, math, phy, chem, cs])
     f.flush()
 
 def print_data():
     f.seek(0)
     data = list(csv.reader(f))
     #print(data, type(data), list(data))
-    print('name', 'roll_no', "English", "Maths", "Physics", "Chemistry", "Computer", end="\t\t")
+    print('Roll_no', 'Name', "English", "Maths", "Physics", "Chemistry", "Computer", end="\t\t")
     print()
     for ls in data[1:]:
         #print(i)
@@ -40,13 +62,16 @@ def rem_student(rele):
     f.seek(0)
     data = list(csv.reader(f))
     for i in range(len(data))[1:]:
-        if int(data[i][1]) == rele:
+        if int(data[i][0]) == rele:
             print('Data of student removed from list-', data.pop(i))
             break
     else:
         print("Roll number not found!")
-        
-    return data
+
+    global classno
+    with open(f"{classno}_Student_List.csv", "w+", newline='') as rwf:
+        csv.writer(rwf).writerows(data)
+    print()
 
 def instruction():
     print('''Welcome to Student Register!
@@ -67,10 +92,7 @@ while True:
     elif n == 2:
         print_data()
     elif n == 3:
-        newls = rem_student(int(input("Enter roll number of student to be removed- ")))
-        with open(f"{classno}_Student_List.csv", "w+", newline='') as rwf:
-            wr.writerows(newls)
-        print()
+        rem_student(int(input("Enter roll number of student to be removed- ")))
     elif n == 4:
         print("Thank you!")
         break
